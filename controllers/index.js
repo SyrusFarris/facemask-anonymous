3cefd706574b9618c8d9802d0096e1f7c846f757
J Litt <jdlitton2594@gmail.com>
2022-08-15 18:35:37 -0500

    route files

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
+
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
