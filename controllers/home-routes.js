167d4958cdd84e9d3064a50887956eb7a77674ff
J Litt <jdlitton2594@gmail.com>
2022-08-16 13:52:02 -0500

    fixed some mistakes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const {} = require('../models');
+// const {} = require('../models');
 
 router.get('/', (req, res) => {
     Review.findAll({
