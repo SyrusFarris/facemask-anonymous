812c87c6ff508d8f18ed4b5b71519e2ba18ae673
J Litt <jdlitton2594@gmail.com>
2022-08-22 20:49:36 -0500

    heroku

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
