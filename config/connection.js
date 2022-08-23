87781cb0631f3cbb7b1e6b8d7e67b654e1c158c5
J Litt <jdlitton2594@gmail.com>
2022-08-22 20:42:30 -0500

    attempt to get heroku to work

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: '0.0.0.0',
       dialect: 'mysql',
       port: 3306
     });
